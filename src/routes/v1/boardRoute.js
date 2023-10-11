0bd3ecdf37ff8a5d63a07ceede0d04de7bb4364f
AnPham <anpham1122000@gmail.com>
2023-10-11 22:46:56 +0700

    init project

---
+++ src/routes/v1/boardRoute.js
@@ -0,0 +1,19 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+
+const Router = express.Router()
+
+Router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ message: 'cc' })
+  })
+  .post((req, res) => {
+    res.status(StatusCodes.CREATED).json({ message: 'board is created successfully' })
+  })
+
+export const boardRoute = Router
--- src/server.js
+++ src/server.js
@@ -1,15 +1,33 @@
-const express = require('express');
+import express from 'express'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
-const app = express();
+const START_SERVER = () => {
+  const app = express()
 
+  app.use('/v1', APIs_V1)
 
-const hostname = 'localhost'
-const port = 8017
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. I am running at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
+  })
 
-app.get('/', function(req, res) {
-    res.send('Hello world')
-})
+  exitHook(() => {
+    console.log('4. Disconnecting to mongodb atlas...')
+    CLOSE_DB()
+  })
+}
 
-app.listen(port, hostname, () => {
-    console.log(`Server is running on port http://${hostname}:${port}`)
-})
+(async () => {
+  try {
+    console.log('1. Connecting to mongodb atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Atlas')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
